1024311de923ad2225029d79a50f0698d381f469
Sam-Github-71 <150336617+Sam-Github-71@users.noreply.github.com>
2024-12-04 10:43:19 +0100

    aggiunto allowed_host

--- primo_progetto/settings.py
+++ primo_progetto/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-@ijl7&u1albur8wyvwm!!+#2pm%22d3(z$x$rh=%=)p7*^#3)n
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
