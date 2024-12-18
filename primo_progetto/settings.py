f5a5216796edfd9048754502d897e12a4d4f23a5
Sam-Github-71 <150336617+Sam-Github-71@users.noreply.github.com>
2024-12-18 10:57:53 +0100

    passo 10 completato

--- primo_progetto/settings.py
+++ primo_progetto/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'prima_app',
+    'seconda_app',
 ]
 
 MIDDLEWARE = [
---
+++ seconda_app/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from prima_app.views import homepage
+from prima_app.views import welcome
+from prima_app.views import lista
+from prima_app.views import chi_siamo
+from prima_app.views import variabili
+from prima_app.views import index
+app_name="seconda_app"
+urlpatterns=[
+  path('es_if', es_if, name='es_if')
+  
+
+]
